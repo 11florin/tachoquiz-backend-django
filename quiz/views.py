2351e8e39bef7281d19e29b604c5d2bd171da581
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-31 10:57:42 +0100

    Added structure static and testing for functionality

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Tacho Quiz is working!")
+    return render(request, "quiz/home.html")
