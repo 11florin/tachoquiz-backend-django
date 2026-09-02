28fa04b9ae1a2fb9f4cebf65e4ed7b8ea153c0e4
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-02 13:25:37 +0100

    Added class Answer in models.py

--- quiz/models.py
+++ quiz/models.py
@@ -17,3 +17,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
