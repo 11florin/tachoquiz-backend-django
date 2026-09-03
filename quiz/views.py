0d7aea151627131ecb11cf6e6a6572dfbde5ab02
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-03 13:35:33 +0100

    Added register page and inegrate registartion validation

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 
 # Create your views here.
 def home(request):
     return render(request, "quiz/home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, "quiz/register.html", {"form": form})
