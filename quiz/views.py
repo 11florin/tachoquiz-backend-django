d7aeecc48fc6c7c6b9b0a67de006f9757433c9be
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-04 14:19:41 +0100

    Added def login_view logic in views.py for login

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 
@@ -23,3 +24,22 @@ def register(request):
 
 def confirmation(request):
     return render(request, "quiz/confirmation.html")
+
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "quiz/login.html", {
+                "error": "Invalid username or password"
+            })
+
+    return render(request, "quiz/login.html")
