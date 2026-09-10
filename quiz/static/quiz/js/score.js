1f8ed33a1c7036564b4f0027e8152eec44a1ae5a
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-10 12:05:54 +0100

    Update score.js

--- quiz/static/quiz/js/score.js
+++ quiz/static/quiz/js/score.js
@@ -3,6 +3,21 @@
 const scoreBar = document.getElementById("score-bar");
 
 if (scoreBar) {
-    const percentage = scoreBar.dataset.percentage;
-    scoreBar.style.width = `${percentage}%`;
+  const percentage = Number.parseInt(
+    scoreBar.dataset.percentage,
+    10
+  );
+
+  if (Number.isInteger(percentage)) {
+    const safePercentage = Math.min(
+      Math.max(percentage, 0),
+      100
+    );
+
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        scoreBar.style.width = `${safePercentage}%`;
+      }, 100);
+    });
+  }
 }
