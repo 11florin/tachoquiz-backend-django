d8606d54d9462657ae87c2db351347db64a2befc
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-16 14:38:06 +0100

    Disable password reset link for deployment

--- tachoquiz/settings.py
+++ tachoquiz/settings.py
@@ -28,6 +28,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
     default="127.0.0.1,localhost",
