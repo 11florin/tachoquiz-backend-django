9cf7e2f276d3f24b98a892912a86ec274abd92b9
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-21 12:24:59 +0100

    test: QuizFunctionality quiz session

---
+++ quiz/tests/test_quiz.py
@@ -0,0 +1,68 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from quiz.models import Category, Question
+
+
+class QuizFunctionalityTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpassword123",
+        )
+
+        self.category = Category.objects.create(
+            name_en="Driving Time",
+            name_ro="Timp de conducere",
+            slug="driving-time",
+        )
+
+        self.question = Question.objects.create(
+            category=self.category,
+            text_en="What is the maximum daily driving time?",
+            text_ro="Care este timpul maxim zilnic de conducere?",
+            explanation_en="Test explanation.",
+            explanation_ro="Explicație de test.",
+        )
+
+        self.client.login(
+            username="testuser",
+            password="testpassword123",
+        )
+
+    def test_starting_quiz_creates_quiz_session(self):
+        response = self.client.get(
+            reverse(
+                "category-quiz",
+                kwargs={"slug": self.category.slug},
+            )
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        session = self.client.session
+
+        self.assertEqual(
+            session["quiz_category_id"],
+            self.category.id
+        )
+
+        self.assertEqual(
+            session["question_index"],
+            0
+        )
+
+        self.assertEqual(
+            session["quiz_answers"],
+            {}
+        )
+
+        self.assertFalse(
+            session["quiz_complete"]
+        )
+
+        self.assertFalse(
+            session["quiz_result_saved"]
+        )
