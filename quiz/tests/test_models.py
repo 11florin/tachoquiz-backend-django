b9d1b0d42746a0f30384d74eaa461882f2c4d4c9
Florin Albu <albuflorin929@yahoo.co.uk>
2026-09-21 10:52:17 +0100

    test: add Question tests

--- quiz/tests/test_models.py
+++ quiz/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils import translation
 
-from quiz.models import Category
+from quiz.models import Category, Question
 
 
 class CategoryModelTest(TestCase):
@@ -30,4 +30,56 @@ class CategoryModelTest(TestCase):
             self.assertEqual(
                 self.category.localized_name,
                 "Timp de conducere"
+            )
+
+
+class QuestionModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            name_en="Driving Time",
+            name_ro="Timp de conducere",
+            slug="driving-time",
+        )
+
+        self.question = Question.objects.create(
+            category=self.category,
+            text_en="What is the maximum daily driving time?",
+            text_ro="Care este timpul maxim zilnic de conducere?",
+            explanation_en="This is the English explanation.",
+            explanation_ro="Aceasta este explicația în limba română.",
+        )
+
+    def test_question_belongs_to_category(self):
+        self.assertEqual(
+            self.question.category,
+            self.category
+        )
+
+    def test_question_localized_text_in_english(self):
+        with translation.override("en"):
+            self.assertEqual(
+                self.question.localized_text,
+                "What is the maximum daily driving time?"
+            )
+
+    def test_question_localized_text_in_romanian(self):
+        with translation.override("ro"):
+            self.assertEqual(
+                self.question.localized_text,
+                "Care este timpul maxim zilnic de conducere?"
+            )
+
+    def test_question_localized_explanation_in_english(self):
+        with translation.override("en"):
+            self.assertEqual(
+                self.question.localized_explanation,
+                "This is the English explanation."
+            )
+
+    def test_question_localized_explanation_in_romanian(self):
+        with translation.override("ro"):
+            self.assertEqual(
+                self.question.localized_explanation,
+                "Aceasta este explicația în limba română."
             )
